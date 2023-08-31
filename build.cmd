7eb8438895bbe41b2f131a74b5a0af79c482579b
nakamura sota <sota.n_100527@icloud.com>
2023-09-01 00:34:42 +0900

    Fix filename

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ Microsoft.Windows.CppWinRT.2.0.230706.1\bin\cppwinrt.exe -input Microsoft.Window
 PyWinRT.1.0.0-beta.10\bin\pywinrt.exe -input Microsoft.WindowsAppSDK.1.4.230822000\lib\uap10.0 -input Microsoft.WindowsAppSDK.1.4.230822000\lib\uap10.0.18362 -input sdk -output pywinrt -module winsdk
 py -m pip install build
 py -m build
-pip install .\dist\winsdk-1.0.0b10-py3-none-any.whl --force-reinstall
+pip install .\dist\winsdk-1.0.0b10-cp311-cp311-win_amd64.whl --force-reinstall
 cl /I "Microsoft.WindowsAppSDK.1.4.230822000\include" /EHsc /std:c++17 /LD MddBootstrap.cpp /link /LIBPATH:"Microsoft.WindowsAppSDK.1.4.230822000\lib\win10-x64" Microsoft.WindowsAppRuntime.Bootstrap.lib Microsoft.WindowsAppRuntime.lib WindowsApp.lib
 copy Microsoft.WindowsAppSDK.1.4.230822000\runtimes\win10-x64\native\Microsoft.WindowsAppRuntime.Bootstrap.dll
 py winui.py
